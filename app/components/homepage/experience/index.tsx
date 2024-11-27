31e7bd955fc2ab65371c1d917e082cfbf8079e60
Junnot Daniel <jerenasonjunnot@gmail.com>
2024-11-27 12:41:40 +0300

    :building_construction: build

--- app/components/homepage/experience/index.tsx
+++ app/components/homepage/experience/index.tsx
@@ -4,7 +4,9 @@ import { experiences } from "@/utils/data/experience";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+import dynamic from "next/dynamic";
+
+const GlowCard = dynamic(() => import('../../helper/glow-card'));
 import experience from '@/public/lottie/code.json';
 
 function Experience() {
