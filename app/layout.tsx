bd31a841b0141ffa961f90e23e069c21a2d8119d
Junnot Daniel <jerenasonjunnot@gmail.com>
2024-11-28 06:08:02 +0300

    :globe_with_meridians: add local-switcher component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 import Footer from "./components/footer";
 import { personalData } from "@/utils/data/personal-data";
 import { ToastContainer } from "react-toastify";
+import { getLocale, getMessages } from "next-intl/server";
+
+import { NextIntlClientProvider } from "next-intl";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,20 +26,25 @@ export const metadata = {
   description: `This is the portfolio of ${personalData.name}. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.`,
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
+  const messages = await getMessages();
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ToastContainer />
         <Navbar />
         <main className="min-h-screen relative mt-14 mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          {children}
+          <NextIntlClientProvider messages={messages}>
+            {children}
+          </NextIntlClientProvider>
           <ScrollToTop />
         </main>
         <Footer />
