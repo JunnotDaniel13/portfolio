35066983cdb86551504cd491d30b29d321e652e4
Junnot Daniel <jerenasonjunnot@gmail.com>
2024-11-30 08:42:43 +0300

    :lipstick: fix about me section responsive design and styling

--- app/components/homepage/about/index.tsx
+++ app/components/homepage/about/index.tsx
@@ -8,7 +8,7 @@ function AboutSection() {
   const t = useTranslations("aboutSection");
   return (
     <div id="about" className="my-12 lg:my-16 relative">
-      <div className="hidden lg:flex flex-col items-center absolute top-16 -right-8">
+      <div className="hidden lg:flex flex-col items-center w-fit absolute top-20 -right-[82px]">
         <span className="bg-[#1a1443] w-fit text-white rotate-90 p-2 px-5 text-xl rounded-md">
           {t("title")}
         </span>
@@ -16,7 +16,7 @@ function AboutSection() {
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
         <div className="order-2 lg:order-1">
-          <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">
+          <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase text-center lg:text-left">
             {t("subtitle")}
           </p>
           <p className="text-gray-200 text-sm lg:text-lg">
@@ -24,13 +24,13 @@ function AboutSection() {
             {t("description")}
           </p>
         </div>
-        <div className="flex justify-center order-1 lg:order-2">
+        <div className="flex justify-center items-center order-1 lg:order-2">
           <Image
             src={personalData.profile}
             priority={false}
             width={280}
             height={280}
-            alt="Abu Said"
+            alt={personalData.name}
             className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
           />
         </div>
