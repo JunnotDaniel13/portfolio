beccc2edf0671ef16c216f4bb0f34aa4a98c3cf3
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-04-26 07:23:44 +0300

    refactor: replace skills data array with categorized skills structure

--- utils/data/skills.ts
+++ utils/data/skills.ts
@@ -1,103 +1,48 @@
-export const skillsData = [
-  'HTML',
-  'CSS',
-  'Javascript',
-  'Typescript',
-  'React',
-  'Next JS',
-  'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'PostgreSQL',
-  'Git',
-  'Docker',
-  'Figma',
-  'MaterialUI',
-]
+import { StandardizedCategorizedSkills } from "@/types/skills";
 
-// Choose your skills from below. Make sure it's in the same format and spelled correctly.
-// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
-
-// AVAILABLE SKILLS
-
-/* 
-  HTML
-  CSS
-  JS 
-  React
-  Next JS
-  Nuxt JS
-  Node JS
-  Vue
-  Angular
-  Docker
-  Photoshop
-  Illustrator
-  Svelte
-  GCP
-  Azure
-  Fastify
-  Haxe
-  Ionic
-  Markdown
-  Microsoft Office
-  Picsart
-  Sketch
-  Unity
-  WolframAlpha
-  Adobe XD
-  After Effects
-  Bootstrap
-  Bulma
-  CapacitorJs
-  Coffeescript
-  MemSQL
-  C
-  C++
-  C#
-  Python
-  Java
-  Julia
-  Matlab
-  Swift
-  Ruby
-  Kotlin
-  Go
-  PHP
-  Flutter
-  Dart
-  Typescript
-  Swift
-  Git
-  Figma
-  Canva
-  Ubuntu
-  Bootstrap
-  MongoDB
-  Tailwind
-  ViteJS
-  VuetifyJS
-  MySQL
-  PostgreSQL
-  AWS
-  Firebase
-  Blender
-  Premiere Pro
-  Adobe Audition
-  Deno
-  Django
-  Gimp
-  Graphql
-  Lightroom
-  MaterialUI
-  Nginx
-  Numpy
-  OpenCV
-  Pytorch
-  Selenium
-  Strapi
-  Tensorflow
-  Webex
-  Wordpress
-*/
+export const categorizedSkills: StandardizedCategorizedSkills = {
+  "Frontend Development": [
+    { label: "HTML5", icon: "HTML5" },
+    { label: "CSS", icon: "CSS" },
+    { label: "React", icon: "ReactDark" },
+    { label: "Nextjs", icon: "Nextjs" },
+    { label: "Angular", icon: "Angular" },
+    { label: "TailwindCSS", icon: "TailwindCSS" },
+    { label: "Bootstrap", icon: "Bootstrap" },
+  ],
+  "Build Tools": [
+    { label: "Vite", icon: "Vite" },
+    { label: "Dotenv", icon: "Dotenv" },
+  ],
+  "Backend Development": [
+    { label: "Nodejs", icon: "Nodejs" },
+    { label: "Expressjs", icon: "ExpressjsDark" },
+    { label: "Hono", icon: "Hono" },
+    { label: "FastAPI", icon: "FastAPI" },
+    { label: "Laravel", icon: "Laravel" },
+  ],
+  Languages: [
+    { label: "JavaScript", icon: "JavaScript" },
+    { label: "TypeScript", icon: "TypeScript" },
+    { label: "Python", icon: "Python" },
+    { label: "Php", icon: "PhpDark" },
+  ],
+  Databases: [
+    { label: "MongoDB", icon: "MongoDB" },
+    { label: "MySQL", icon: "MySQL" },
+    { label: "PostgreSQL", icon: "PostgreSQL" },
+    { label: "SQLite", icon: "SQLite" },
+  ],
+  "Tools & DevOps": [
+    { label: "Git", icon: "Git" },
+    { label: "Docker", icon: "Docker" },
+    { label: "Linux", icon: "Linux" },
+    { label: "Ubuntu", icon: "Ubuntu" },
+    { label: "Windows", icon: "Windows" },
+  ],
+  Design: [
+    { label: "Canva", icon: "Canva" },
+    { label: "Figma", icon: "Figma" },
+    { label: "Photoshop", icon: "Photoshop" },
+  ],
+};
